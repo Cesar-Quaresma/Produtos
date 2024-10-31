7d78b81f96fd821cddb942ff97ace3c35f84bcf0
Cesar-Quaresma <tharkwz@gmail.com>
2024-10-31 20:30:52 -0300

    Update Produtos.sql

--- Produtos.sql
+++ Produtos.sql
@@ -3,40 +3,40 @@ Create Table Produto (
 id_Produto int primary key not null,
 Nome_Produto varchar (30) not null,
 Marca varchar(27),
-PreÁo money,
+Pre√ßo money,
 Categoria varchar (21),
 Data_Vencimento date not null,
-N˙mero_SÈrie char (27) );
+N√∫mero_S√©rie char (27) );
 
-Create Table ImportaÁ„o_A (
-id_ImportaÁ„o_A int primary key not null,
+Create Table Importa√ß√£o_A (
+id_Importa√ß√£o_A int primary key not null,
 Nome_Estabelecimento varchar (33) not null,
-Previs„o_Chegada_Produto date,
+Previs√£o_Chegada_Produto date,
 Tel_Fixo char(15),
 Tel_Celular_ou_WhatsApp char(18),
 Localidade varchar (45) not null,
 Cidade varchar (27) not null,
-Regi„o varchar (27) not null );
+Regi√£o varchar (27) not null );
 
-Create Table ExportaÁ„o_A(
-id_ExportaÁ„o_A int primary key not null,
+Create Table Exporta√ß√£o_A(
+id_Exporta√ß√£o_A int primary key not null,
 Nome_Transportadora varchar (30) not null,
-Data_SaÌda_da_Transportadora date,
+Data_Sa√≠da_da_Transportadora date,
 Data_Entrega Date not null,
 Tel_Fixo char(15),
 Tel_Celular_ou_WhatsApp char (18),
 Localidade_Transportadora varchar (39),
 Cidade varchar (27),
-Regi„o varchar(27) );
+Regi√£o varchar(27) );
 
 Alter Table Produto
-Add Constraint produ_Import foreign key (id_Produto) References ImportaÁ„o_A (id_ImportaÁ„o_A);
+Add Constraint produ_Import foreign key (id_Produto) References Importa√ß√£o_A (id_Importa√ß√£o_A);
 
 Alter Table Produto
-Add Constraint produ_Export foreign key (id_Produto) References ExportaÁ„o_A (id_ExportaÁ„o_A);
+Add Constraint produ_Export foreign key (id_Produto) References Exporta√ß√£o_A (id_Exporta√ß√£o_A);
 
-Alter Table ImportaÁ„o_A
-Add Constraint Import_Export_Produtos foreign key (id_ImportaÁ„o_A) References ExportaÁ„o_A (id_ExportaÁ„o_A);
+Alter Table Importa√ß√£o_A
+Add Constraint Import_Export_Produtos foreign key (id_Importa√ß√£o_A) References Exporta√ß√£o_A (id_Exporta√ß√£o_A);
 
 
 
